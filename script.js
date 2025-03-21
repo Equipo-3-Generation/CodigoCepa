c603cf30bfc22d4ebaca6b58c000ec2823858231
Ana <ana.lucia.villalobos.rivas@gmail.com>
2025-03-21 17:43:16 -0600

    Modificar el estilo y la posición de elementos del formulario

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const inputFile = document.getElementById('formFile');
+    const imagePreview = document.getElementById('imagePreview');
+    
+    inputFile.addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            imagePreview.style.display = 'block'; // Muestra la imagen
+            imagePreview.src = e.target.result; // Asigna la URL generada al src de la imagen
+        };
+
+        if (file) {
+            reader.readAsDataURL(file); // Convierte el archivo a una URL y la asigna a la imagen
+        }
+    });
+});
