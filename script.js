d0fc694d9fcdd373fe91a5c6e60a7f830c7de12c
Ana <ana.lucia.villalobos.rivas@gmail.com>
2025-03-21 18:31:32 -0600

    Fusionar página de productos y añadir productos. Cambios en nombres de archivos

--- script.js
+++ script.js
@@ -38,3 +38,26 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 //aquí termina el formulario de contacto <3
+
+// --> Función para vista previa de imágenes del formulario de creación de objetos
+
+document.addEventListener('DOMContentLoaded', function () {
+    const inputFile = document.getElementById('formFile');
+    const imagePreview = document.getElementById('imagePreview');
+    
+    inputFile.addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            imagePreview.style.display = 'block'; // Muestra la imagen
+            imagePreview.src = e.target.result; // Asigna la URL generada al src de la imagen
+        };
+
+        if (file) {
+            reader.readAsDataURL(file); // Convierte el archivo a una URL y la asigna a la imagen
+        }
+    });
+});
+
+// Fin de la función
